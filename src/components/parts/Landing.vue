7a52c43ab2f55a86fb8b1e186d99eca3df47f6c1
brenbread <santos.brenj@gmail.com>
2021-02-09 20:46:07 -0800

    Updated landing info

--- src/components/parts/Landing.vue
+++ src/components/parts/Landing.vue
@@ -1,22 +1,24 @@
 <template>
     <!-- Start landing page text element at 0% opacity  -->
-    <!-- <div id="landing" v-anime="{ loop: false, duration: 2000, easing: 'linear' }"> -->
+
     <md-content class="md-elevation-5">
-    <div id="landing">
-        <span id="msg">Hi there, I'm Bren!</span>
-        <br />
-        <span id="msg">I graduated with a BS in Computer Engineering from SJSU.</span>
-        <br />
-        <span id="msg">I have industry experience in QA automation, testing methodologies, and have a high attention to detail.</span>
-        <br />
-        <span id="msg">I also have experience with full-stack web development and embedded development as well.</span>
-        <br />
-        <span id="msg">On the side, I like rhythm games (mostly Bemani), music, mechanical keyboards, playing the guitar, the occasional anime, and 🍛 <a href="https://twitter.com/brenbread_/status/1129478927523598337" target="_blank">CURRY</a> 🍛!!</span>
-        <br />
-        <span id="msg"><b>Currently looking for opportunities</b>, so drop me a message <a :href="lnk_linkedin" target="_blank">here</a> if you're interested in talking!</span>
-        <br />
-        <SocialMedia/>
-    </div>
+        <div id="landing">
+            <span>Hi there, I'm Bren!</span>
+            <br />
+            <span>I'm currently working as a Member of Technical Staff @ ONF.</span>
+            <br />
+            <span>I graduated with a BS in Computer Engineering from SJSU.</span>
+            <br />
+            <span>I have industry experience in QA automation, testing methodologies, and have a high attention to detail.</span>
+            <br />
+            <span>I also have experience with full-stack web development and embedded development.</span>
+            <br />
+            <span>On the side, I like rhythm games (mostly Bemani), music, mechanical keyboards, playing the guitar, the occasional anime, and 🍛 <a href="https://twitter.com/brenbread_/status/1129478927523598337" target="_blank">CURRY</a> 🍛!!</span>
+            <br />
+            <span>Not looking for opportunities at the moment.</span>
+            <br />
+            <SocialMedia/>
+        </div>
     </md-content>
 </template>
 
@@ -52,7 +54,7 @@ export default {
     data: function() {
         return {
             // LinkedIn link for landing
-            lnk_linkedin: socialMedia.socialMedia.LinkedIn,
+            lnk_linkedin: socialMedia.socialMedia.LinkedIn
         }
     }
 }
